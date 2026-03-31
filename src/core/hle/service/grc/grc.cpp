5b01fe859e29d7b849f448d67972f03532decfdb
Ploo <239304139+xinitrcn1@users.noreply.github.com>
2026-03-31 04:31:39 +0000

    hle: grc implement stubs (#104)
    Co-authored-by: lizzie <lizzie@eden-emu.dev>

--- src/core/hle/service/grc/grc.cpp
+++ src/core/hle/service/grc/grc.cpp
@@ -4,12 +4,77 @@
 #include <memory>
 
 #include "core/hle/service/cmif_serialization.h"
-#include "core/hle/service/grc/continuous_recorder.h"
 #include "core/hle/service/grc/grc.h"
 #include "core/hle/service/server_manager.h"
 #include "core/hle/service/service.h"
+#include "core/hle/service/cmif_serialization.h"
+#include "core/hle/service/cmif_types.h"
 
 namespace Service::GRC {
+
+class IContinuousRecorder final : public ServiceFramework<IContinuousRecorder> {
+public:
+    IContinuousRecorder(Core::System& system_)
+        : ServiceFramework{system_, "IContinuousRecorder"} {
+        // clang-format off
+        static const FunctionInfo functions[] = {
+            {0, D<&IContinuousRecorder::StopRecording>, "StopRecording"},
+            {4, D<&IContinuousRecorder::UpdateRecordingStartTick>, "UpdateRecordingStartTick"},
+            {5, D<&IContinuousRecorder::Unknown5>, "Unknown5"}, // [20.2.0+] Takes 0x20-byte input
+            {10, D<&IContinuousRecorder::GetNotFlushingEvent>, "GetNotFlushingEvent"},
+            {11, D<&IContinuousRecorder::StartFlush>, "StartFlush"},
+            {12, D<&IContinuousRecorder::CancelFlush>, "CancelFlush"},
+            {13, D<&IContinuousRecorder::ResetFlushTime>, "ResetFlushTime"},
+            {14, D<&IContinuousRecorder::StartFlushWithEvent>, "StartFlushWithEvent"},
+        };
+        // clang-format on
+        RegisterHandlers(functions);
+    }
+
+    ~IContinuousRecorder() = default;
+
+private:
+    Result StopRecording() {
+        LOG_WARNING(Service_GRC, "(STUBBED) called");
+        R_SUCCEED();
+    }
+
+    Result UpdateRecordingStartTick() {
+        LOG_WARNING(Service_GRC, "(STUBBED) called");
+        R_SUCCEED();
+    }
+
+    // [20.2.0+] Takes 0x20-byte input
+    Result Unknown5(std::array<char, 0x20> in_buffer) {
+        LOG_WARNING(Service_GRC, "(STUBBED) called");
+        R_SUCCEED();
+    }
+
+    Result GetNotFlushingEvent(OutCopyHandle<Kernel::KReadableEvent> out_event) {
+        LOG_WARNING(Service_GRC, "(STUBBED) called");
+        R_SUCCEED();
+    }
+
+    Result StartFlush(InBuffer<BufferAttr_HipcPointer> in_buffer) {
+        LOG_WARNING(Service_GRC, "(STUBBED) called");
+        R_SUCCEED();
+    }
+
+    Result CancelFlush() {
+        LOG_WARNING(Service_GRC, "(STUBBED) called");
+        R_SUCCEED();
+    }
+
+    Result ResetFlushTime(u64 in_time) {
+        LOG_WARNING(Service_GRC, "(STUBBED) called");
+        R_SUCCEED();
+    }
+
+    Result StartFlushWithEvent(InBuffer<BufferAttr_HipcPointer> in_buffer, OutCopyHandle<Kernel::KReadableEvent> out_event) {
+        LOG_WARNING(Service_GRC, "(STUBBED) called");
+        R_SUCCEED();
+    }
+};
 
 class GRC final : public ServiceFramework<GRC> {
 public:
