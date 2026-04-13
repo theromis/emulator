5461afc163544bd6b8621f9156996aafcdf1debc
Mythrax <mythrax@mytrax-rs.org>
2026-04-13 18:18:42 +1000

    feat: Add FastRMX title ID

--- src/citron/util/title_ids.h
+++ src/citron/util/title_ids.h
@@ -13,6 +13,7 @@ private:
     TitleID() = default;
 
 public:
+    static constexpr u64 FastRMX = 0x01009510001CA000ULL;
     static constexpr u64 FinalFantasyTactics = 0x010038B015560000ULL;
     static constexpr u64 LittleNightmares3Base = 0x010066101A55A000ULL;
     static constexpr u64 MarvelCosmicInvasion = 0x010059D020C26000ULL;
